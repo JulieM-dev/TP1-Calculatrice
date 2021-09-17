7245d2f38c87109dedc1b6ce01970faf86844fd7
JulieM-dev <julie.neko.egirl@gmail.com>
2021-09-17 16:01:26 +0200

    Front modification style boutons

--- app/src/main/java/com/example/tp1_calculatrice/MainActivity.kt
+++ app/src/main/java/com/example/tp1_calculatrice/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.calculator)
 
         var tableChiffres = arrayOf (
             findViewById<Button>(R.id.but0),
